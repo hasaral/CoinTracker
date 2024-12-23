edae65793b2735f00bbd2d16e4fc96324b4caba0
hasaral <hasaral@gmail.com>
2024-12-23 21:16:40 +0400

    Initial Commit

---
+++ CoinTracker/CoinTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrackerApp.swift
+//  CoinTracker
+//
+//  Created by Hasan Saral on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
